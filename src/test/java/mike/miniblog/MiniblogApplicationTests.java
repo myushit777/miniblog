7fee3496cd9ad3740f306618fbc519252883ab89
myushit777 <myushit777@gmail.com>
2023-10-27 12:57:30 +0200

    v1.0

---
+++ src/test/java/mike/miniblog/MiniblogApplicationTests.java
@@ -0,0 +1,13 @@
+package mike.miniblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
