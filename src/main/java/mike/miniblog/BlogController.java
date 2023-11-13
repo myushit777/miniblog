438f5c7e113b81c9a94baf57a91d207cee7a38b7
myushit777 <myushit777@gmail.com>
2023-11-13 18:02:15 +0100

    DONE

--- src/main/java/mike/miniblog/BlogController.java
+++ src/main/java/mike/miniblog/BlogController.java
@@ -1,32 +1,29 @@
 package mike.miniblog;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class BlogController {
 
     private final BlogService blogService; // BlogService für Geschäftslogik :)
 
+    @Autowired
     public BlogController(BlogService blogService) {
         this.blogService = blogService;
     }
 
     // HTTP GET-Endpunkt -> zum Abrufen von Blog-Einträgen
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping
     public List<BlogEntry> getBlogEntries() {
         return blogService.getAllBlogEntries();
     }
 
     // HTTP POST-Endpunkt -> zum Erstellen eines neuen Blog-Eintrags
-    @PostMapping("/")
-    @ResponseBody
+    @PostMapping
     public BlogEntry createBlogEntry(@RequestBody BlogEntry entry) {
         return blogService.createBlogEntry(entry);
     }
